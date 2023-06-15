9cffa1c9854b3e0e844bad4b3808bc32fc4727d5
Velecia-M <veleciamokoena@gmail.com>
2023-06-15 12:49:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
 /**
  * main - A program that prints a line to the sandard error
  *
- * Return: Always 0 (Success)
- */
+ * Return 1 (Success)
+ * */
 int main(void)
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
