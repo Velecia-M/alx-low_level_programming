fb79454daa9ea03880bd81b42f1770677f6ad060
Velecia-M <veleciamokoena@gmail.com>
2023-06-17 21:03:42 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 and 9 and letter between a and f
+ * Return: Always 0
+ */
+int main(void)
+{
+int x;
+
+for (x = 48; x < 58; i++)
+{
+putchar(x);
+}
+for (x = 97; x < 103; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
