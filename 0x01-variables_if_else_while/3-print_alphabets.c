6668e92e6dea606e2f7f433cb5b89d384c2b8cb1
Velecia-M <veleciamokoena@gmail.com>
2023-06-17 19:55:37 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase and uppercase
+ * Return: Always 0
+ */
+int main(void)
+{
+char x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+
+for (i = 0; i < 52; i++)
+{
+putchar(x[i]);
+}
+putchar('\n');
+return (0);
+}
