c81aa967c90ba074d280d5ed85ae2904db78a15f
Velecia-M <veleciamokoena@gmail.com>
2023-06-20 07:07:04 -0700

    void print_alphabet(void);

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints  the alphabet in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+char alph;
+
+for (alph = 'a'; alph <='z'; alph++)
+_putchar(alph);
+_putchar('\n');
+}
