d02765098b096374eeded7a10d7de50b0adfbceb
Velecia-M <veleciamokoena@gmail.com>
2023-06-28 14:42:08 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: pointer to string params
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+int x, y;
+char val1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char valrot[] = "NOPQRSTUVWXYZABCDEFGHIKLMnopqrstuvwxyzabcdefghijklm";
+
+for (x = 0; s[x] != '\0'; x++)
+{
+for (y = 0; y <  52; y++)
+if (s[x] == val1[y])
+s[x] = valrot[y];
+break;
+}
+}
+}
+return (s);
+}
