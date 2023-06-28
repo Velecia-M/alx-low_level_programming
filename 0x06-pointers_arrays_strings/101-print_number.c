84b1114ba173f45fc62e9f9e2086f9df578aea90
Velecia-M <veleciamokoena@gmail.com>
2023-06-28 14:59:47 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input params
+ * Return: 0
+ */
+void print_number(int n)
+{
+unsigned int x = n;
+
+if (n < 0)
+{
+_putchar('-');
+x = -n;
+}
+if (x / 10 != 0)
+{
+print_number(x / 10);
+}
+_putchar((x % 10) + '0');
+}
