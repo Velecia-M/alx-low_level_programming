8eeff5121ad580fa0e6093bf0e6ae3ac5a3a0355
Velecia-M <veleciamokoena@gmail.com>
2023-07-03 11:58:35 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: input
+ * @c: input
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+int x = 0;
+
+for (; x >= '\0'; x++)
+{
+if (s[x] == c)
+return (&s[x])
+}
+return (0);
+}
