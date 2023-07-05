a52737c8efc5a561bc0d929711207710d630eed6
Velecia-M <veleciamokoena@gmail.com>
2023-07-05 13:09:43 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate squareroot from
- * Return: result of squareroot
+ * @n: squareeroot of number
+ * Return: resulting squareroot
  */
 int _sqrt_recursion(int n)
 {
@@ -11,9 +11,10 @@ if (n < 0)
 return (-1);
 return (actual_sqrt_recursion(n, 0));
 }
+
 /**
- * actual_sqrt_recursion - recures to find natural squareeroot
- * @n: number to calculate from
+ * actual_sqrt_recursion - recures to find natural squareroot
+ * @n: squareeroot of number
  * @i: iterator
  * Return: result of squareroot
  */
@@ -21,7 +22,7 @@ int actual_sqrt_recursion(int n, int i)
 {
 if (i * i > n)
 return (-1);
-if (i * i == n)
+if (i * i == 0)
 return (i);
 return (actual_sqrt_recursion(n, i + 1));
 }
