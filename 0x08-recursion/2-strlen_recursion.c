1fc7c6654fcdeb94ea6336a74a37fb3071a63d27
Velecia-M <veleciamokoena@gmail.com>
2023-07-05 12:23:13 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+int x;
+
+if (*s)
+{
+x++;
+x += _strlen_recursion(s + 1);
+}
+return (x);
+}
