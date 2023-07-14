5931c0a5edab63fe5551d4d9b5b0fc7a17e61aa5
Velecia-M <veleciamokoena@gmail.com>
2023-07-13 18:36:08 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *strng;
+char *s;
 unsigned int x = 0, y = 0, len1 = 0, len2 = 0;
 
 while (s1 && s1[len2])
@@ -18,20 +18,20 @@ len1++;
 while (s2 && s2[len2])
 len2++;
 if (n < len2)
-strng = malloc(sizeof(char) * (len1 + n + 1));
+s = malloc(sizeof(char) * (len1 + n + 1));
 else
-strng = malloc(sizeof(char) * (len1 + len2 + 1));
-if (!strng)
+s = malloc(sizeof(char) * (len1 + len2 + 1));
+if (!s)
 return (NULL);
 while (x < len1)
 {
-strng = s1[x];
+s[x] = s1[x];
 x++;
 }
 while (n < len1 && (len1 + n))
-strng[x++] = s2[y++];
+s[x++] = s2[y++];
 while (n >= len2 && x < (len1 + len2))
-strng[x++] = s2[y++];
-strng[x] = '\0';
-return (strng);
+s[x++] = s2[y++];
+s[x] = '\0';
+return (s);
 }
