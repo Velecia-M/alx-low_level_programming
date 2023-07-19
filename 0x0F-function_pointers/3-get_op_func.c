2b03ecd34eb01f9b1774b1e12b09cb608ec0ef08
Velecia-M <veleciamokoena@gmail.com>
2023-07-19 14:48:01 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,13 +8,12 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
+op_t ops[] = {{"+", op_add},
 {"-", op_sub},
 {"*", op_mul},
 {"/", op_div},
 {"%", op_mod},
-{NULL, NULL},};
+{NULL, NULL}, };
 
 int x = 0;
 while (ops[x].op != NULL && *(ops[x].op) != *s)
